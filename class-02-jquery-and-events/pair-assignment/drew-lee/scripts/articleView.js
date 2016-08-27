c80ff91b57d034dc474ab95aa071dd374530fe6f
DrewStock <andrewdstock@gmail.com>
2016-08-27 16:29:54 -0700

    completed all TODO items for class-02 pair assignment

--- class-02-jquery-and-events/pair-assignment/drew-lee/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/drew-lee/scripts/articleView.js
@@ -32,13 +32,18 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      //       DONE: Drew & Lee
+      var $selectedAuthor = $('article[data-author="' + $(this).val()+ '"]');
+      $('article').hide();
+      $selectedAuthor.fadeIn("slow");
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      //       DONE: Drew & Lee
+      $('article').show();
+      $('article.template').hide();
     }
-    $('#category-filter').val('');
+    $('#author-filter').val('');
   });
 };
 
@@ -46,7 +51,26 @@ articleView.handleCategoryFilter = function() {
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
-  //       Be sure to reset the #author-filter while you are at it!
+  //       DONE: Drew & Lee
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      //       and then show just the ones that match for the author that was selected.
+      //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      //       DONE: Drew & Lee
+      var $selectedCategory = $('article[data-category="' + $(this).val()+ '"]');
+      $('article').hide();
+      $selectedCategory.fadeIn("slow");
+    } else {
+      // TODO: If the select box was changed to an option that is blank, we should
+      //       show all the articles, except the one article we are using as a template.
+      //       DONE: Drew & Lee
+      $('article').show();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+
 
 };
 
@@ -56,21 +80,40 @@ articleView.handleMainNav = function() {
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
   //       data available to you on the .tab element that was clicked.
-  $('.main-nav').on(/* CODE GOES HERE */);
-
+  //       DONE: Drew & Lee
+  $('.main-nav .tab').on('click', function() {
+    var tab_id = $(this).attr('data-content');
+    var selectedTab = $('#' + tab_id);
+    $('section.tab-content').hide();
+    selectedTab.show();
+  });
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 articleView.setTeasers = function() {
-  $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
+  $('.article-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any article body.
 
   // TODO: Add an event handler to reveal all the hidden elements,
   //       when the .read-on link is clicked. You can go ahead and hide the
   //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
   //       process any .read-on clicks that happen within child nodes.
-
+  //       DONE: Drew & Lee
+  $('section#articles .read-on').on('click', function(e) {
+    e.preventDefault();
+    var readOnLink = $(e.target);
+    readOnLink.hide();
+    readOnLink.parent().find('p').fadeIn("slow");
+  });
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
-$();
+//       DONE: Drew & Lee
+
+$(document).ready(function() {
+   articleView.populateFilters();
+   articleView.handleAuthorFilter();
+   articleView.handleCategoryFilter();
+   articleView.setTeasers();
+   articleView.handleMainNav();
+});
