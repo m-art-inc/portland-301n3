c05f51da3f374d09585ce6704f140d810e563ec3
Rosemary Joe <jjj430@msn.com>
2016-08-29 16:50:59 -0700

    update style

--- class-02-jquery-and-events/pair-assignment/jon-rosemary-cheryl/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/jon-rosemary-cheryl/scripts/articleView.js
@@ -25,25 +25,18 @@ articleView.populateFilters = function() {
     }
   });
 };
-
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-	  
-	  console.log($(this).val());
-	  
-	  
     if ($(this).val()) {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-	  
 		var $selectedAuthor = $('article[data-attribute="' + $(this).val() +'"]');
 		$('article').not($selectedAuthor).hide();
 		} else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
 	    $('article').not('.template').show();
-		
     }
     $('#author-filter').val('');
   });
@@ -65,11 +58,9 @@ articleView.handleCategoryFilter = function() {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
 	    $('article').not('.template').show();
-		
     }
     $('#category-filter').val('');
   });
-
 };
 
 articleView.handleMainNav = function() {
