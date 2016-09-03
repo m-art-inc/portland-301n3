ae41b3ade3b851079764c25a70903c3dfdb1ef07
Rosemary Joe <jjj430@msn.com>
2016-09-03 14:11:47 -0700

    fix  articles.js return

--- class-04-templates/pair-assignment/greg-rosemary/scripts/article.js
+++ class-04-templates/pair-assignment/greg-rosemary/scripts/article.js
@@ -15,8 +15,9 @@ Article.prototype.toHtml = function() {
   //       - Now "compile" your template with Handlebars.
   var appTemplate = $("#template").html();
   var compileTemplate = Handlebars.compile(appTemplate);
-  var html = compileTemplate(rawData);
   
+  
+
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -25,6 +26,7 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  return compileTemplate(this);
 };
 
 rawData.sort(function(a,b) {
